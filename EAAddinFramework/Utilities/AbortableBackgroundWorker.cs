dcc8ef180544b1ab97d6619a4381f3c1cda8c217
GeertBellekens <geert@bellekens.com>
2012-11-16 05:20:39 +0100

    added AbortableBackgroundWorker.cs

---
+++ EAAddinFramework/Utilities/AbortableBackgroundWorker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading;
+
+
+namespace EAAddinFramework.Utilities
+{
+	/// <summary>
+	/// Description of AbortableBackgroundWorker.
+	/// borrowed from http://stackoverflow.com/questions/800767/how-to-kill-background-worker-completely 
+	/// </summary>
+public class AbortableBackgroundWorker : BackgroundWorker
+{
+
+    private Thread workerThread;
+
+    protected override void OnDoWork(DoWorkEventArgs e)
+    {
+        workerThread = Thread.CurrentThread;
+        try
+        {
+            base.OnDoWork(e);
+        }
+        catch (ThreadAbortException)
+        {
+            e.Cancel = true; //We must set Cancel property to true!
+            Thread.ResetAbort(); //Prevents ThreadAbortException propagation
+        }
+    }
+
+
+    public void Abort()
+    {
+        if (workerThread != null)
+        {
+            workerThread.Abort();
+            workerThread = null;
+        }
+    }
+}
+}
