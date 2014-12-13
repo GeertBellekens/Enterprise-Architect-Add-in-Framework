e79d6a6539ad9b556f57ecfd90c2026fc710c892
GeertBellekens <geert@bellekens.com>
2014-12-13 09:21:23 +0100

    moved over to logicNP for key handling

--- EAAddinFramework/Licensing/License.cs
+++ EAAddinFramework/Licensing/License.cs
@@ -9,6 +9,7 @@
 using System;
 using System.Security.Cryptography;
 using System.Text;
+using LogicNP.CryptoLicensing;
 
 namespace EAAddinFramework.Licensing
 {
@@ -18,8 +19,6 @@ namespace EAAddinFramework.Licensing
 	public class License
 	{
 		private string _key;
-		public string application {get;set;}
-		public DateTime validUntil {get;set;}
 		public bool floating {get;set;}
 		public string client {get;set;}
 		public bool isValid {get;private set;}
@@ -35,56 +34,24 @@ namespace EAAddinFramework.Licensing
 			}
 		}
 		
-		public License(string application, DateTime validUntil, bool floating, string client)
-		{
-			this.application = application;
-			this.validUntil = validUntil;
-			this.floating = floating;
-			this.client = client;
-		}
 		public License(string key, string publicKey)
 		{
-			DSACryptoServiceProvider provider = new DSACryptoServiceProvider();
-			provider.FromXmlString(publicKey);
-			string[] keyParts = key.Split('|');
-			if (keyParts.Length == 2)
+			
+			this.key = key;
+			//create the wrapped license
+			CryptoLicense wrappedlicense = new CryptoLicense(key, publicKey);
+			//validate the signature
+			this.isValid = (wrappedlicense.Status == LicenseStatus.Valid);
+			//get user data
+			if (this.isValid)
 			{
-				string data = keyParts[0];
-				string signature = keyParts[1];
-				//decode the signature
-				signature = Encoding.Unicode.GetString(Convert.FromBase64String(signature));
-				//validate the signature
-				this.isValid = provider.VerifyData(Encoding.Unicode.GetBytes(data),Encoding.Unicode.GetBytes(signature));
-				//decode the contens of the key if the key was valid
-				if (this.isValid)
+				this.client = wrappedlicense.GetUserDataFieldValue("Client", "|");
+				string isFloating = wrappedlicense.GetUserDataFieldValue("Isfloating","|");
+				bool isFloatingBool;
+				if(bool.TryParse(isFloating,out isFloatingBool))
 				{
-					if (data.IndexOf("-") > 0) //in case of a floating license the first part is "ApplicationName-" in plain text
-					{
-						string[] dataparts = data.Split('-');
-						data = dataparts[0] + "|" + Encoding.Unicode.GetString(Convert.FromBase64String(dataparts[1]));
-						
-					}else
-					{
-						data = Encoding.Unicode.GetString(Convert.FromBase64String(data));
-					}
-					//split up the data
-					string[] datasplitted = data.Split('|');
-					if (datasplitted.Length == 4)
-					{
-						this.application = datasplitted[0];
-						DateTime validDate;
-						if (DateTime.TryParse(datasplitted[1], out validDate))
-					    {
-					    	this.validUntil = validDate;
-					    }
-						this.client = datasplitted[2];
-						//datasplitted[3] is the guid, which we don't need right now
-					}
+					this.floating = isFloatingBool;
 				}
-				
-				
-				
-				
 				
 			}
 			
