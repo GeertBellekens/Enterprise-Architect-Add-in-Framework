82ad81b0a097f8757d2da2b10e5c8b8cae742372
Geert Bellekens <geert@bellekens.com>
2018-10-23 09:11:46 +0200

    Improved performance in getting mapping logic

--- EAAddinFramework/Mapping/ConnectorMapping.cs
+++ EAAddinFramework/Mapping/ConnectorMapping.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using MP = MappingFramework;
-using UML=TSF.UmlToolingFramework.UML;
-using TSF.UmlToolingFramework.Wrappers.EA;
-using System.Collections.Generic;
 using System.Linq;
-using TFS_EA = TSF.UmlToolingFramework.Wrappers.EA;
+using TSF.UmlToolingFramework.Wrappers.EA;
+using MP = MappingFramework;
 
 namespace EAAddinFramework.Mapping
 {
-	/// <summary>
-	/// Description of ConnectorMapping.
-	/// </summary>
-	public class ConnectorMapping:Mapping
-	{
-		internal ConnectorWrapper wrappedConnector{get;private set;}
+    /// <summary>
+    /// Description of ConnectorMapping.
+    /// </summary>
+    public class ConnectorMapping : Mapping
+    {
+        internal ConnectorWrapper wrappedConnector { get; private set; }
         public ConnectorMapping(ConnectorWrapper wrappedConnector, MappingNode sourceEnd, MappingNode targetEnd) : base(sourceEnd, targetEnd)
         {
             this.wrappedConnector = wrappedConnector;
@@ -21,56 +18,63 @@ namespace EAAddinFramework.Mapping
 
         #region implemented abstract members of Mapping
 
-        public override MP.MappingLogic mappingLogic {
-			get 
-			{
-				//check via linked element
-				if (_mappingLogic == null)
-				{
-					//check links to notes or constraints
-					//TODO: make a difference between regular notes and mapping logic?
-					var connectedElement = this.wrappedConnector.getLinkedElements().OfType<ElementWrapper>().FirstOrDefault(x => x.notes.Length > 0);
-					if (connectedElement != null) _mappingLogic = new MappingLogic(connectedElement);
-				}
-				// check via tagged value
-				if (_mappingLogic == null)
-				{
-					var mappingtag = this.wrappedConnector.taggedValues
-						.FirstOrDefault( x => x.name.Equals(MappingFactory.mappingLogicName,StringComparison.InvariantCultureIgnoreCase));
-					if (mappingtag != null && 
-					    ! string.IsNullOrEmpty(mappingtag.tagValue.ToString()))
-					{
-						_mappingLogic = new MappingLogic(mappingtag.tagValue.ToString());
-					}
-				}
-				return _mappingLogic;
-			}
-			set 
-			{
-				var mappingElementWrapper = value.mappingElement as ElementWrapper;
-				if (mappingElementWrapper != null)
-				{
-					this.wrappedConnector.addTaggedValue(MappingFactory.mappingLogicName, mappingElementWrapper.uniqueID);
-					//TODO get this working for at least notes and constraints, for now we go with a tagged value
-					// this.wrappedConnector.addLinkedElement(mappingElementWrapper);
-				}
-				else
-				{
-					this.wrappedConnector.addTaggedValue(MappingFactory.mappingLogicName, value.description);
-				}
-				
-			}
-		}
+        public override MP.MappingLogic mappingLogic
+        {
+            get
+            {
+                //check via linked element
+                //TODO: implement in a faster way if mapping via elements is supported again
+                //if (this._mappingLogic == null)
+                //{
+                //    //check links to notes or constraints
+                //    //TODO: make a difference between regular notes and mapping logic?
+                //    var connectedElement = this.wrappedConnector.getLinkedElements().OfType<ElementWrapper>().FirstOrDefault(x => x.notes.Length > 0);
+                //    if (connectedElement != null)
+                //    {
+                //        this._mappingLogic = new MappingLogic(connectedElement);
+                //    }
+                //}
+                // check via tagged value
+                if (this._mappingLogic == null)
+                {
+                    var mappingtag = this.wrappedConnector.taggedValues
+                        .FirstOrDefault(x => x.name.Equals(MappingFactory.mappingLogicName, StringComparison.InvariantCultureIgnoreCase));
+                    if (mappingtag != null &&
+                        !string.IsNullOrEmpty(mappingtag.tagValue.ToString()))
+                    {
+                        this._mappingLogic = new MappingLogic(mappingtag.tagValue.ToString());
+                    }
+                }
+                return this._mappingLogic;
+            }
+            set
+            {
+                var mappingElementWrapper = value.mappingElement as ElementWrapper;
+                if (mappingElementWrapper != null)
+                {
+                    this.wrappedConnector.addTaggedValue(MappingFactory.mappingLogicName, mappingElementWrapper.uniqueID);
+                    //TODO get this working for at least notes and constraints, for now we go with a tagged value
+                    // this.wrappedConnector.addLinkedElement(mappingElementWrapper);
+                }
+                else
+                {
+                    this.wrappedConnector.addTaggedValue(MappingFactory.mappingLogicName, value.description);
+                }
 
-		#endregion
+            }
+        }
 
-		#region implemented abstract members of Mapping
+        #endregion
 
-		public override void save()
-		{
+        #region implemented abstract members of Mapping
+
+        public override void save()
+        {
             if (this._mappingLogic != null)
+            {
                 this.mappingLogic = this._mappingLogic; //make sure to set the mapping logic value correctly
-                                                        //set mapping path
+            }
+            //set mapping path
             if (this.source.structure == MP.ModelStructure.Message)
             {
                 this.wrappedConnector.addTaggedValue(MappingFactory.mappingSourcePathName, string.Join(".", ((MappingNode)this.source).getMappingPath()));
@@ -80,7 +84,7 @@ namespace EAAddinFramework.Mapping
                 this.wrappedConnector.addTaggedValue(MappingFactory.mappingTargetPathName, string.Join(".", ((MappingNode)this.target).getMappingPath()));
             }
             this.wrappedConnector.save();
-		}
+        }
 
         public override void deleteWrappedItem()
         {
