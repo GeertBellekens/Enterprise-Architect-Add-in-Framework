45f0f29bee2b133898c460c50992e3c172985bc2
Geert Bellekens <geert@bellekens.com>
2020-02-28 11:55:44 +0100

    Added error/warning handling to output logger

--- EAAddinFramework/Utilities/EAOutputLogger.cs
+++ EAAddinFramework/Utilities/EAOutputLogger.cs
@@ -12,7 +12,10 @@ namespace EAAddinFramework.Utilities
 	{
 		Model model;
 		string name;
-		static Dictionary<string, EAOutputLogger> outputLogs = new Dictionary<string, EAOutputLogger>();
+        List<string> errors = new List<string>();
+        List<string> warnings = new List<string>();
+
+        static Dictionary<string, EAOutputLogger> outputLogs = new Dictionary<string, EAOutputLogger>();
 		static EAOutputLogger defaultLogger;
 		static EAOutputLogger getOutputLogger(Model model, string outputName)
 		{
@@ -24,12 +27,22 @@ namespace EAAddinFramework.Utilities
 			defaultLogger = outputLogs[logKey]; 
 			return defaultLogger;
 		}
-		/// <summary>
-		/// private constructor
-		/// </summary>
-		/// <param name="model">the model this output applies to</param>
-		/// <param name="outputName"></param>
-		private EAOutputLogger(Model model, string outputName)
+        public static IEnumerable<string> getErrors (Model model, string outputName)
+        {
+            var outputLogger = getOutputLogger(model, outputName);
+            return outputLogger.errors;
+        }
+        public static IEnumerable<string> getWarnings(Model model, string outputName)
+        {
+            var outputLogger = getOutputLogger(model, outputName);
+            return outputLogger.warnings;
+        }
+        /// <summary>
+        /// private constructor
+        /// </summary>
+        /// <param name="model">the model this output applies to</param>
+        /// <param name="outputName"></param>
+        private EAOutputLogger(Model model, string outputName)
 		{
 			this.model = model;
 			this.name = outputName;
@@ -38,6 +51,16 @@ namespace EAAddinFramework.Utilities
 			this.model.wrappedModel.EnsureOutputVisible(this.name);
 			this.model.wrappedModel.ClearOutput(this.name);
 		}
+        private void logError(string message, int elementID = 0)
+        {
+            this.logToOutput(message, elementID);
+            this.errors.Add(message);
+        }
+        private void logWarning(string message, int elementID = 0)
+        {
+            this.logToOutput(message, elementID);
+            this.warnings.Add(message);
+        }
         private void logToOutput(string message, int elementID = 0)
 		{
 			this.model.wrappedModel.WriteOutput(this.name,message,elementID);
@@ -45,6 +68,8 @@ namespace EAAddinFramework.Utilities
 		private void clear()
 		{
 			this.model.wrappedModel.ClearOutput(this.name);
+            this.errors.Clear();
+            this.warnings.Clear();
 		}
 		/// <summary>
 		/// log a message to the EA output window. If requested the message will also be logged to the logfile
@@ -63,18 +88,22 @@ namespace EAAddinFramework.Utilities
 			{
 				case LogTypeEnum.log:
 					Logger.log(message);
-					break;
+                    //log to output
+                    logger.logToOutput($"{DateTime.Now.ToString("hh:mm:ss.fff")} {message}", elementID);
+                    break;
 				case LogTypeEnum.warning:
 					message = "Warning: " + message;
 					Logger.logWarning(message);
-					break;
+                    //log to output
+                    logger.logWarning($"{DateTime.Now.ToString("hh:mm:ss.fff")} {message}", elementID);
+                    break;
 				case LogTypeEnum.error:
 					message = "Error: " + message;
 					Logger.logError(message);
-					break;
+                    //log to output
+                    logger.logError($"{DateTime.Now.ToString("hh:mm:ss.fff")} {message}", elementID);
+                    break;
 			}
-			//log to output
-			logger.logToOutput($"{DateTime.Now.ToString("hh:mm:ss.fff")} {message}",elementID);
 		}
 		/// <summary>
 		/// logs a message to the currently active outputlogger
