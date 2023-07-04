3d7f45b4ddea3e4b6da8191a836b6490564c4192
Geert Bellekens <geert@bellekens.com>
2023-07-04 13:22:20 +0200

    Added ProgressBarWindow()

---
+++ EAAddinFramework/Utilities/ProgressBarWindow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAAddinFramework.Utilities
+{
+    public partial class ProgressBarWindow : Form
+    {
+        private string command;
+        private string arguments;
+        public ProgressBarWindow()
+        {
+            Application.EnableVisualStyles();
+            InitializeComponent();
+        }
+        public string execute(string command, string arguments, string title, string label)
+        {
+            this.Text = title;
+            this.label.Text = label;
+            this.command = command;
+            this.arguments = arguments;
+            this.ShowDialog();
+            return this.outputTextBox.Text;
+        }
+        private void runCommand(string command, string arguments)
+        {
+            var processStartInfo = new ProcessStartInfo();
+            processStartInfo.FileName = command;
+            processStartInfo.Arguments = arguments;
+            processStartInfo.UseShellExecute = false;
+            processStartInfo.RedirectStandardOutput = true;
+            var process = new Process();
+            process.StartInfo = processStartInfo;
+            process.Start();
+            this.outputTextBox.Text = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+        }
+
+        private async void ProgressBarWindow_Shown(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(this.command))
+            {
+                await Task.Run(() => { this.runCommand(this.command, this.arguments); });
+                this.label.Text = "Finished";
+                this.progressBar.Style = ProgressBarStyle.Continuous;
+                this.progressBar.MarqueeAnimationSpeed = 0;
+            }
+        }
+    }
+}
--- EAAddinTester/EAAddinTesterProgram.cs
+++ EAAddinTester/EAAddinTesterProgram.cs
@@ -141,24 +141,26 @@ namespace EAAddinTester
         /// <summary>
         /// generic test method. To be filled in with whatever needs to be tested.
         /// </summary>
-        internal static void myTest()
+        internal static void myTest(string command, string arguments)
         {
-            EAWrappers.Model model = new EAWrappers.Model();
-            var selectedPackage = model.selectedTreePackage as EAWrappers.Package;
-            var outputName = "EATester";
-            //test regular getting all elements
-            //EAOutputLogger.clearLog(model, outputName);
-            //EAOutputLogger.log(model, outputName, $"starting regular test for package '{selectedPackage?.name}'", 0);
-            //var elements = selectedPackage.getAllOwnedElements();
-            //EAOutputLogger.log(model, outputName, $"found {elements.Count} new elements", 0);
-            var connector = model.getRelationByGUID("{B34E3FAE-B047-46be-B4BD-EFA5D1DD1B27}");
-            var sourceEnd = connector.sourceEnd;
-            foreach(var tag in sourceEnd.taggedValues)
-            {
-                EAOutputLogger.log(model, outputName, $"found {tag.name} with value {tag.tagValue} ", 0);
-                tag.tagValue = "12";
-                tag.save();
-            }
+            var progresBarWindows = new ProgressBarWindow();
+            progresBarWindows.execute(command, arguments, "executing command title", "currently running this command");
+            //EAWrappers.Model model = new EAWrappers.Model();
+            //var selectedPackage = model.selectedTreePackage as EAWrappers.Package;
+            //var outputName = "EATester";
+            ////test regular getting all elements
+            ////EAOutputLogger.clearLog(model, outputName);
+            ////EAOutputLogger.log(model, outputName, $"starting regular test for package '{selectedPackage?.name}'", 0);
+            ////var elements = selectedPackage.getAllOwnedElements();
+            ////EAOutputLogger.log(model, outputName, $"found {elements.Count} new elements", 0);
+            //var connector = model.getRelationByGUID("{B34E3FAE-B047-46be-B4BD-EFA5D1DD1B27}");
+            //var sourceEnd = connector.sourceEnd;
+            //foreach(var tag in sourceEnd.taggedValues)
+            //{
+            //    EAOutputLogger.log(model, outputName, $"found {tag.name} with value {tag.tagValue} ", 0);
+            //    tag.tagValue = "12";
+            //    tag.save();
+            //}
 
         }
         /// <summary>
