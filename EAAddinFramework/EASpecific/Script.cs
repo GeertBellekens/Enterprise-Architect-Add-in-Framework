946e1a9d2f2f8efc1d0509babf14bff068e1c863
Barrie Treloar <baerrach@gmail.com>
2023-08-12 06:37:46 +0930

    executeFunction handle exceptions and include script name

--- EAAddinFramework/EASpecific/Script.cs
+++ EAAddinFramework/EASpecific/Script.cs
@@ -405,7 +405,14 @@ namespace EAAddinFramework.EASpecific
         /// <returns>whathever (if anything) the function returns</returns>
         internal object executeFunction(string functionName, object[] parameters)
         {
-            return this.scriptController.Run(functionName, parameters);
+            try
+            {
+                return this.scriptController.Run(functionName, parameters);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Script '{fullyQualifiedName}' failed: {ex.Message}", ex);
+            }
         }
         /// <summary>
         /// executes the function with the given name
@@ -414,7 +421,7 @@ namespace EAAddinFramework.EASpecific
         /// <returns>whathever (if anything) the function returns</returns>
         internal object executeFunction(string functionName)
         {
-            return this.scriptController.Run(functionName, new object[0]);
+            return executeFunction(functionName, new object[0]);
         }
         /// <summary>
         /// add a function with based on the given operation
